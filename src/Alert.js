5344f027339ca95ec646e318cb38b5efe7a57044
David J Crockett <crockett.davidj@gmail.com>
2022-11-27 16:43:45 -0800

    Initial React Setup

---
+++ src/Alert.js
@@ -0,0 +1,7 @@
+import React, { useEffect } from 'react'
+
+const Alert = () => {
+  return <h2>Hello, Alert!</h2>
+}
+
+export default Alert;
---
+++ src/List.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { FaEdit, FaTrash } from 'react-icons/fa'
+const List = () => {
+  return <h2>Hello, List!</h2>
+}
+
+export default List;
