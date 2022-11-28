755c81aecedb3e5ebcdb66fd08e250ec78a3e487
David J Crockett <crockett.davidj@gmail.com>
2022-11-27 18:32:57 -0800

    Finished structuring

--- src/App.js
+++ src/App.js
@@ -8,8 +8,28 @@ function App() {
   const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditID] = useState(null);
   const [alert, setAlert] = useState({show:false, msg:'', type:'' });
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('test')
+  }
 
   return <section className="section-center">
+    <form className="fruit-form" onSubmit={handleSubmit}>
+      {alert.show && <Alert/>}
+      <h3>Fruit Collection</h3>
+      <div className="form-control">
+        <input 
+          type='text'
+          className='fruit'
+          placeholder='example: Pineapple'
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button type="submit" className="submit-btn">
+          {isEditing? 'edit': 'Submit'}
+        </button>
+      </div>
+    </form>
     <div className="fruit-container">
       <List />
       <button className="clear-btn">Clear Fruits</button>
